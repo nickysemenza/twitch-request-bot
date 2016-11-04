9f79c5fdfe24e6c857aed00bba505826ae92e765
Nicky Semenza <nicky@nickysemenza.com>
2016-11-04 00:02:40 -0400

    some basic Models

---
+++ database/migrations/2016_11_03_200752_create_song_requests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSongRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('song_requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('youtube_url');
+            $table->string('notes');
+            $table->string('instrument');
+            $table->boolean('priority')->default(false);
+            $table->boolean('played')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('song_requests');
+    }
+}
