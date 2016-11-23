ac7ffd4b2e94bdf5cbafcca737ee1574c861f8a8
Nicky Semenza <nicky@nickysemenza.com>
2016-11-23 12:25:46 -0800

    songqueue style temporary

--- src/js/components/SongQueue.js
+++ src/js/components/SongQueue.js
@@ -1,41 +1,21 @@
 import React, { Component } from 'react';
 import {Table} from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
 
 export default class SongQueue extends Component {
 
   render() {
-
-    var tbody;
+    var list;
     if(this.props.queue)
-      tbody = this.props.queue.map(function(song){
-        return (<tr key={song.id}>
-          <td>{song.id}</td>
-          <td>{song.title}</td>
-          <td>{song.youtube_id}</td>
-          <td>{song.priority}</td>
-          <td>{song.status}</td>
-          <td>{song.user.username}</td>
-          <td>
-            {song.status==0 ? <button className="button-primary-wide" onClick={() => this.props.play(song.id)}>play now</button> : ''}
-          </td>
-        </tr>)
+      list = this.props.queue.map(function(song){
+        return (<div key={song.id} className={song.status==1 ? 'songListElement songListElement-nowPlaying' : 'songListElement'}>
+          <a href={"https://www.youtube.com/watch?v="+song.youtube_id} target="_blank">{song.title}</a>
+          <p>requested by {song.user.username} {song.priority ? <FontAwesome name='star' /> : ''}</p>
+          {song.status==0 ? <button className="button-primary" onClick={() => this.props.play(song.id)}>play now</button> : ''}
+          <p>(id={song.id})</p>
+        </div>);
       },this);
 
-    return(<Table bordered condensed>
-      <thead>
-      <tr>
-        <th>id</th>
-        <th>Song</th>
-        <th>ytid</th>
-        <th>priority</th>
-        <th>status</th>
-        <th>requested by</th>
-        <th>play now</th>
-      </tr>
-      </thead>
-      <tbody>
-      {tbody}
-      </tbody>
-    </Table>);
+    return(<div>{list}</div>);
   }
 }
