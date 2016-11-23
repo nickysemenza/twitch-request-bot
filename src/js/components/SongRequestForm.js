3d9bd838ae60c92260107cf65153d5307d75c4b8
Nicky Semenza <nicky@nickysemenza.com>
2016-11-23 12:27:46 -0800

    request form improvements

--- src/js/components/SongRequestForm.js
+++ src/js/components/SongRequestForm.js
@@ -1,36 +1,43 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-
+import { Field, reduxForm, formValueSelector } from 'redux-form';
+import { connect } from 'react-redux'
 
 const validate = values => {
   const errors = {}
   if (!values.youtube_url) {
     errors.youtube_url = 'Required'
-  // } else if (!/^http:\/\/(?:www\.)?youtube.com\/watch\?(?=.*v=\w+)(?:\S+)?$/i.test(values.youtube_url)) {
-  //   errors.youtube_url = 'Invalid email address'
+  } else if (!/^http:\/\/(?:www\.)?youtube.com\/watch\?(?=.*v=\w+)(?:\S+)?$/i.test(values.youtube_url)) {
+    errors.youtube_url = 'Invalid youtube link'
+    //TODO: make this support https youtbez
   }
-
   return errors
 }
 
+const renderField = ({ input, label, type, meta: { touched, error } }) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type}/>
+      {touched && error && <span>{error}</span>}
+    </div>
+  </div>
+)
 
-class SongRequestForm extends Component {
-  render() {
-    const { handleSubmit } = this.props;
+let SongRequestForm = (props) => {
+  const { error, handleSubmit, pristine, reset, submitting, pointsTotal, valid} = props
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} style={{color: "black"}}>
         <div>
-          <label style={{color:'black'}} htmlFor="youtube_url">Youtube URL:</label>
-          <Field style={{color:'black'}} name="youtube_url" component="input" type="text"/>
+          <Field name="youtube_url" component={renderField} type="text" label="Youtube URL"/>
         </div>
         <div>
-          <label style={{color:'black'}} htmlFor="use_priority">Make Request Priority?  (+5pts)</label>
+          <label htmlFor="use_priority">Make Request Priority?  (+5pts)</label>
           <Field name="use_priority" component="input" type="checkbox"/>
         </div>
         <div>
-          <label style={{color:'black'}} htmlFor="instrument">Select Instrument (+5pts)</label>
+          <label htmlFor="instrument">Select Instrument (+5pts)</label>
           <div>
-            <Field style={{color:'black'}} name="instrument" component="select">
+            <Field name="instrument" component="select">
               <option value="none">None</option>
               <option value="piano">Piano</option>
               <option value="guitar">Guitar</option>
@@ -38,16 +45,42 @@ class SongRequestForm extends Component {
             </Field>
           </div>
         </div>
-        <button className="button-primary-wide" type="submit">Submit</button>
+        <pre>
+          {error}
+          </pre>
+        <p>You have {props.creditBalance} request credits. {(pointsTotal > props.creditBalance) ? "Looks like you need more!" : ""}</p>
+
+          <button type="submit" disabled={pristine || submitting || !valid || (pointsTotal > props.creditBalance)}>Submit for {pointsTotal} points</button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
       </form>
     );
-  }
 }
 
-// Decorate the form component
+// // Decorate the form component
 SongRequestForm = reduxForm({
   form: 'songrequest', // a unique name for this form
   validate,
 })(SongRequestForm);
 
+const selector = formValueSelector('songrequest') // <-- same as form name
+SongRequestForm = connect(
+  state => {
+    // can select values individually
+    var pointsTotal = 0;
+    if(selector(state, 'use_priority'))
+      pointsTotal += 5;
+    let instrument = selector(state, 'instrument');
+    if(instrument != "none" && instrument !== undefined)
+      pointsTotal += 5;
+    return {
+      pointsTotal
+    }
+  }
+)(SongRequestForm)
+
+
 export default SongRequestForm;
+// export default reduxForm({
+//   form: 'songrequest',
+//   validate
+// })(SongRequestForm)
