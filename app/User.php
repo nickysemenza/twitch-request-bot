27869e3f05347126526fe92487a1a4ecfb894b30
Nicky Semenza <nicky@nickysemenza.com>
2016-11-25 00:00:47 -0800

    decrement points for song requests, allows updating of song requests

--- app/User.php
+++ app/User.php
@@ -31,46 +31,63 @@ class User extends Authenticatable
     public function hasUnplayedSong() {
         return SongRequest::where('user_id',$this->id)->where('status','!=',SongRequest::PLAYED)->count() > 0;
     }
+    public function getUnplayedSongRequest() {
+        return SongRequest::where('user_id',$this->id)->where('status','!=',SongRequest::PLAYED)->first();
+    }
+    public function useCredits($amt,$info = null) {
+        if($amt > $this->credits)
+            return false;
+        $t = new CreditTransaction();
+        $t->user_id = $this->id;
+        $t->credit_used = $amt;
+        $t->info = json_encode($info);
+        $t->save();
+        $this->credits -= $amt;
+        $this->save();
+        Log::info("used {$amt} credits for user {$this->id}");
+        return true;
+    }
 
     public function requestSong($youtube_url, $is_priority = false, $options = null) {
-        $user = $this;
         //TODO: validate youtube_url
         //return ['status'=>'error','error'=>'invalid youtube link'];
 
+        $isUpdateRequest = $this->hasUnplayedSong();
         $numCreditsUsed = 0;
 
         $youtube_id = GeneralController::getYoutubeVideoID($youtube_url);
         $sr = new SongRequest();
-        $sr->user_id = $user->id;
+        if($isUpdateRequest)
+            $sr = $this->getUnplayedSongRequest();
+        $sr->user_id = $this->id;
         $sr->youtube_id = $youtube_id;
         $sr->title = GeneralController::getYoutubeTitle($youtube_id);
 
         if($is_priority) {
-            $numCreditsUsed+=5;
+            //we want to use credit for new priority request, or editing a request to make it now priority
+            if(($isUpdateRequest && !$sr->priority) || !$isUpdateRequest)
+                $numCreditsUsed+=5;
             $sr->priority = true;
         }
 
-        //todo: verify instrument is valid
         if(isset($options['instrument'])) {
             $instrument = $options['instrument'];
-            if ($instrument != "none") {
+            if ($instrument != "none" && $instrument != "") {
+                if(!$isUpdateRequest || ($isUpdateRequest && !$sr->instrument))
                 $numCreditsUsed += 5;
                 $sr->instrument = $instrument;
             }
         }
 
-        Log::info("need credits:".$numCreditsUsed." have: ".$user->credits);
-        if($user->credits>=$numCreditsUsed) {
-            $sr->save();
+        $sr->save();
+        Log::info("need credits:".$numCreditsUsed." have: ".$this->credits);
+        if($this->useCredits($numCreditsUsed,['song_request'=>$sr->id])) {
             return ['status'=>'ok'];
         }
         else {
+            $sr->delete();//backtrack!
             return ['status'=>'error','error'=>'not enough credits'];
         }
-        //TODO: actually decrement the points
-
-        //TODO: replace unplayed song
-
 
     }
 }
