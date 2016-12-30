1394126d8b2fbb29594df262c685305410d912aa
Nicky Semenza <nicky@nickysemenza.com>
2016-12-30 01:07:41 -0800

    fix test for 500 credit change

--- tests/UsersTest.php
+++ tests/UsersTest.php
@@ -26,23 +26,23 @@ class UsersTest extends TestCase
         $username = $faker->username;
         $user = UsersController::getByName($username);
         $user = \App\User::find($user->id);
-        $user->giveCredits(6, ['k'=>'v']);
+        $user->giveCredits(600, ['k'=>'v']);
         $user = $user->fresh();
-        $this->assertTrue($user->credits == 6);
-        $this->seeInDatabase('credit_transactions', ['user_id' => $user->id, 'credit_gained'=>6, 'info'=> json_encode(['k'=>'v'])]);
+        $this->assertTrue($user->credits == 600);
+        $this->seeInDatabase('credit_transactions', ['user_id' => $user->id, 'credit_gained'=>600, 'info'=> json_encode(['k'=>'v'])]);
 
-        $use4 = $user->useCredits(4, ['a'=>'b']);
+        $use4 = $user->useCredits(400, ['a'=>'b']);
         $this->assertTrue($use4);
-        $this->assertTrue($user->credits == 2);
-        $this->seeInDatabase('credit_transactions', ['user_id' => $user->id, 'credit_used'=>4, 'info'=> json_encode(['a'=>'b'])]);
-        $use3 = $user->useCredits(3, ['a'=>'b']);
+        $this->assertTrue($user->credits == 200);
+        $this->seeInDatabase('credit_transactions', ['user_id' => $user->id, 'credit_used'=>400, 'info'=> json_encode(['a'=>'b'])]);
+        $use3 = $user->useCredits(300, ['a'=>'b']);
         $this->assertFalse($use3);
-        $this->assertTrue($user->credits == 2);
+        $this->assertTrue($user->credits == 200);
 
         $this->assertTrue($user->requestSong('https://www.youtube.com/watch?v=HBT2J4tMyGI', true) == ['status'=>'error', 'error'=>'not enough credits']);
         $this->assertTrue($user->requestSong('https://www.youtube.com/watch?v=HBT2J4tMyGI', false) == ['status'=>'ok']);
 
-        $user->giveCredits(6, ['k'=>'v']);
+        $user->giveCredits(600, ['k'=>'v']);
 
         $this->assertTrue($user->requestSong('https://www.youtube.com/watch?v=85ftfVUTzM4', false) == ['status'=>'ok']);
         $this->assertTrue($user->getUnplayedSongRequest()->instrument == null);
