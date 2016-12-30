c7e5fd9d3d221e7adbb5e673b93de76379f6d32a
Nicky Semenza <nicky@nickysemenza.com>
2016-12-29 22:05:12 -0800

    Eslint

--- server.js
+++ server.js
@@ -1,50 +1,48 @@
-var irc = require('irc');
-var request = require('request');
-var express = require('express')
-var bodyParser = require('body-parser')
+const irc = require('irc');
+const request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var settings = require('./settings');
+const settings = require('./settings');
 
-var app = express()
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+const app = express();
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+  extended: true,
+}));
 
-var client = new irc.Client(settings.server, settings.botName, {
-  channels: [settings.channels + " " + settings.password],
+const client = new irc.Client(settings.server, settings.botName, {
+  channels: [`${settings.channels} ${settings.password}`],
   debug: true,
   password: settings.password,
   username: settings.botName,
 });
 
-client.addListener('error', function(message) {
-    console.log('error: ', message);
+client.addListener('error', (message) => {
+  console.log('error: ', message);
 });
 
 /* when we receive a message from IRC, forward it off to laravel */
-client.addListener('message', function (from, to, message) {
-	console.log(from, to, message);
-  
+client.addListener('message', (from, to, message) => {
+  console.log(from, to, message);
+
   request({
-    url: settings.api_server_address+"/irc/message",
+    url: `${settings.api_server_address}/irc/message`,
     method: 'POST',
-    json: {'message':message,'sender':from,'token':settings.internal_token}
-  }, function(error, response, body){
+    json: { message, sender: from, token: settings.internal_token },
+  }, (error, response, body) => {
     console.log(body);
   });
-
 });
 
 /* when we receive a message (to send) from laravel, send it off to IRC */
-app.post('/msg', function (req, res) {
-  //TODO: authenticate csrf using settings.internal_token	
+app.post('/msg', (req, res) => {
+  // TODO: authenticate csrf using settings.internal_token
   client.say(settings.channels[0], req.body.msg);
   res.send(200);
-})
+});
 
-app.listen(settings.relay_port, function () {
-  console.log('IRC relay listening on port '+settings.relay_port);
-})
-
+app.listen(settings.relay_port, () => {
+  console.log(`IRC relay listening on port ${settings.relay_port}`);
+});
 
--- settings.example.js
+++ settings.example.js
@@ -1,9 +1,9 @@
 module.exports = {
-  channels: ["#channel"],
-  server: "irc.twitch.tv",
-  botName: "",
-  password: "",
-  internal_token: "asdfjkha",
-  relay_port: "3000",
-  api_server_address: ""
-}
+  channels: ['#channel'],
+  server: 'irc.twitch.tv',
+  botName: '',
+  password: '',
+  internal_token: 'asdfjkha',
+  relay_port: '3000',
+  api_server_address: '',
+};
