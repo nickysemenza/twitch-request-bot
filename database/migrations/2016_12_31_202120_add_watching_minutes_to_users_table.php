46c82c90651cf32d510700a4214c9e56732502db
Nicky Semenza <nicky@nickysemenza.com>
2016-12-31 12:29:58 -0800

    columns for number of minutes watched

---
+++ database/migrations/2016_12_31_202120_add_watching_minutes_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWatchingMinutesToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('minutes_watched')->default(0);
+            $table->integer('minutes_watched_active')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('minutes_watched');
+            $table->dropColumn('minutes_watched_active');
+        });
+    }
+}
