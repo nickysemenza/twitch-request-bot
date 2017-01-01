c46bbcd13e670936fca5d376159efb169259aef0
Nicky Semenza <nicky@nickysemenza.com>
2017-01-01 09:18:58 +0000

    Apply fixes from StyleCI

--- app/Stream.php
+++ app/Stream.php
@@ -8,12 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Stream extends Model
 {
     /**
-     * Gets the active stream_id, or null
-     * @return integer|null
+     * Gets the active stream_id, or null.
+     * @return int|null
      */
     public static function getActiveID()
     {
-        $stream = Stream::where('streaming',true)->first();
+        $stream = self::where('streaming', true)->first();
+
         return $stream ? $stream->id : null;
     }
 
@@ -21,14 +22,16 @@ class Stream extends Model
      * Starts a stream session, if no active one exists.
      * @return bool success
      */
-    public static function startSession() {
-        if(self::getActiveID() != null) {
+    public static function startSession()
+    {
+        if (self::getActiveID() != null) {
             //there's already an active session
             return false;
         }
-        $session = new Stream();
+        $session = new self();
         $session->streaming = true;
         $session->save();
+
         return true;
     }
 
@@ -36,16 +39,18 @@ class Stream extends Model
      * Finishes a stream session, if there is an active one.
      * @return bool
      */
-    public static function finishSession() {
+    public static function finishSession()
+    {
         $active_id = self::getActiveID();
-        if($active_id == null) {
+        if ($active_id == null) {
             //there's not an active session!
             return false;
         }
-        $active = Stream::find($active_id);
+        $active = self::find($active_id);
         $active->streaming = false;
         $active->finished_at = Carbon::now();
         $active->save();
+
         return true;
     }
 }
--- tests/StreamSessionTest.php
+++ tests/StreamSessionTest.php
@@ -1,9 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Stream;
+
 class StreamSessionTest extends TestCase
 {
     /**
