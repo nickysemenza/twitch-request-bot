b956d110ddbe2d00f68d2f8800af1b296a6e7966
Nicky Semenza <nicky@nickysemenza.com>
2017-01-01 01:18:51 -0800

    Ability to start/finish stream sessions

---
+++ tests/StreamSessionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Stream;
+class StreamSessionTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testStartFinishStream()
+    {
+        //clear, reset
+        Stream::finishSession();
+        //now there is definitely not one active
+        //try finishing an already finished
+        self::assertFalse(Stream::finishSession());
+        //now let's start a new session
+        $new = Stream::startSession();
+        self::assertTrue($new);
+        //so we shouldn't be able to restart...
+        self::assertFalse(Stream::startSession());
+
+        //but we can finish!
+        self::assertTrue(Stream::finishSession());
+        //but only once...
+        self::assertFalse(Stream::finishSession());
+    }
+}
